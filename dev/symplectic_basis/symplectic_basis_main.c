9c0449d0128dbc55678c13e64c1fe9645004322e
JWonderchilds <joshua.childs01@outlook.com>
2023-03-16 22:08:46 +1100

    matrix pipeline for symplectic basis

--- dev/symplectic_basis/symplectic_basis_main.c
+++ dev/symplectic_basis/symplectic_basis_main.c
@@ -8,19 +8,59 @@
 
 #include "SnapPea.h"
 #include "unix_cusped_census.h"
+//#include "addl_code.h"                  // SnapPy
+#include "../addl_code/addl_code.h"     // Kernel
 #include <stdio.h>
 
+void printMatrix(int**, int, int);
+void mergeRows(Triangulation *);
 
 int main(void) {
+
     Triangulation *theTriangulation;
 
-    theTriangulation = GetCuspedCensusManifold("", 5, oriented_manifold, 125);
+    theTriangulation = GetCuspedCensusManifold("", 5, oriented_manifold, 4);
 
     if (theTriangulation != NULL) {
-        printf("%s  %d\n", get_triangulation_name(theTriangulation), get_symplectic_basis(theTriangulation));
-        free_triangulation(theTriangulation);
+        mergeRows(theTriangulation);
     } else
         printf("Couldn't read census manifold.\n");
 
     return 0;
+}
+
+
+void mergeRows(Triangulation *manifold) {
+    int numRows, numCols, dualRows;
+    int** eqns;
+
+    // Edge Equations
+    eqns = get_gluing_equations(manifold, &numRows, &numCols);
+    printMatrix(eqns, numCols, numRows);
+    free_gluing_equations(eqns, numRows);
+
+    // Cusp Equations
+
+
+    // Dual Equations
+    eqns = get_symplectic_basis(manifold, &dualRows);
+    printMatrix(eqns, numCols, dualRows);
+    free_gluing_equations(eqns, dualRows);
+
+    free_triangulation(manifold);
+}
+
+
+void printMatrix(int** M, int numCols, int numRows) {
+    int i, j;
+
+    for (i = 0; i < numRows; i ++) {
+        printf("[");
+
+        for (j = 0; j < numCols - 1; j ++) {
+            printf(" %d,", M[i][j]);
+        }
+
+        printf(" %d]\n", M[i][numCols - 1]);
+    }
 }
--- kernel/kernel_code/symplectic_basis.c
+++ kernel/kernel_code/symplectic_basis.c
@@ -7,7 +7,23 @@
 #include "kernel.h"
 #include "kernel_namespace.h"
 
-int get_symplectic_basis(Triangulation *manifold)
+int** get_symplectic_basis(Triangulation *manifold, int* dual_rows)
 {
-    return 100;
+    int i, j, T;
+    int** eqns;
+
+    T = manifold -> num_tetrahedra;
+
+    *dual_rows = 2;
+
+    eqns = NEW_ARRAY(*dual_rows, int*);
+
+    for (i = 0; i < *dual_rows; i ++) {
+        eqns[i] = NEW_ARRAY(3*T, int);
+
+        for (j = 0; j < 3 * T; j ++)
+            eqns[i][j] = i + j;
+    }
+
+    return eqns;
 }
