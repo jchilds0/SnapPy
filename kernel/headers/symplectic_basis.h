87451536483cccc8ec1efcffc211534e6fd6e6ce
JWonderchilds <joshua.childs01@outlook.com>
2023-03-21 11:56:44 +1100

    Breadth First Search

--- kernel/headers/symplectic_basis.h
+++ kernel/headers/symplectic_basis.h
@@ -10,44 +10,43 @@
 #include "triangulation.h"
 #include <stdbool.h>
 
+#define     MAXV    10
+
 struct queue {
     int front;      // First element of queue
     int rear;       // Last element of queue
     int len;        // num of elements
     int size;       // array size
-    struct Tetrahedron **array;
+    int *array;
 };
 
 void initialise_queue(struct queue *, int);
-struct queue *enqueue(struct queue *, Tetrahedron *);
-Tetrahedron *dequeue(struct queue *);
+struct queue *enqueue(struct queue *, int);
+int dequeue(struct queue *);
 void resize_queue(struct queue *);
 int empty_queue(struct queue *);
 void free_queue(struct queue *);
 
-//#define MAXV   100
-//
-//bool processed[MAXV + 1];
-//bool discovered[MAXV + 1];
-//int parent[MAXV + 1];
-//
-//typedef struct edgenode {
-//    int y;
-//    int weight;
-//    struct edgenode *next;
-//} edgenode;
-//
-//typedef struct {
-//    struct edgenode *edges[MAXV + 1];
-//    int degree[MAXV + 1];
-//    int nvertices;
-//    int nedges;
-//    int directed;
-//} graph;
-//
-//void initialise_search(graph *);
-//void process_vertex_early(int);
-//void process_edge(int, int);
-//void process_vertex_late(int);
-//void find_path(int, int, int[]);
-//void insert_edge(graph *, int, int, bool);
+typedef struct edgenode {
+    int y;
+    int weight;
+    struct edgenode *next;
+} edgenode;
+
+typedef struct {
+    struct edgenode *edges[MAXV];
+    int degree[MAXV];
+    int nvertices;
+    int nedges;
+    int directed;
+} graph;
+
+void initialise_graph(graph *, int, int, bool);
+void free_graph(graph *);
+void initialise_search(graph *, bool *, bool *, int *);
+void bfs(graph *, int, bool *, bool *, int *);
+void process_vertex_early(int);
+void process_edge(int, int);
+void process_vertex_late(int);
+void find_path(int, int, int *, int *, int);
+void insert_edge(graph *, int, int, bool);
