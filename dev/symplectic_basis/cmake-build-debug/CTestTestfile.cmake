f8feaf2c864a87f72dabbbb8fb91a97b9f9f1f11
Josh <joshua.childs01@outlook.com>
2023-03-29 11:53:35 +1100

    renaming structs

--- dev/symplectic_basis/cmake-build-debug/CTestTestfile.cmake
+++ dev/symplectic_basis/cmake-build-debug/CTestTestfile.cmake
@@ -1,8 +1,8 @@
 # CMake generated Testfile for 
-# Source directory: C:/Users/joshu/Documents/1. School Stuff/Monash 2023/Honours - Research Project/development/SnapPy/dev/symplectic_basis
-# Build directory: C:/Users/joshu/Documents/1. School Stuff/Monash 2023/Honours - Research Project/development/SnapPy/dev/symplectic_basis/cmake-build-debug
+# Source directory: C:/Users/joshu/Documents/Monash/Honours - Research Project/development/SnapPy/dev/symplectic_basis
+# Build directory: C:/Users/joshu/Documents/Monash/Honours - Research Project/development/SnapPy/dev/symplectic_basis/cmake-build-debug
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(symplectic_test "C:/Users/joshu/Documents/1. School Stuff/Monash 2023/Honours - Research Project/development/SnapPy/dev/symplectic_basis/cmake-build-debug/symplectic_basis_test.exe")
-set_tests_properties(symplectic_test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/joshu/Documents/1. School Stuff/Monash 2023/Honours - Research Project/development/SnapPy/dev/symplectic_basis/CMakeLists.txt;43;add_test;C:/Users/joshu/Documents/1. School Stuff/Monash 2023/Honours - Research Project/development/SnapPy/dev/symplectic_basis/CMakeLists.txt;0;")
+add_test(symplectic_test "C:/Users/joshu/Documents/Monash/Honours - Research Project/development/SnapPy/dev/symplectic_basis/cmake-build-debug/symplectic_basis_test.exe")
+set_tests_properties(symplectic_test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/joshu/Documents/Monash/Honours - Research Project/development/SnapPy/dev/symplectic_basis/CMakeLists.txt;43;add_test;C:/Users/joshu/Documents/Monash/Honours - Research Project/development/SnapPy/dev/symplectic_basis/CMakeLists.txt;0;")
